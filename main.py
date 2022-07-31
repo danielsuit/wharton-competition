422725a995e3307af65dde67cc08468e1132a886
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-07-30 20:57:22 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def main(x):
-   return x
-if __name__ == '__main__':
-   print(main(1))
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+# def main(x):
+   # return x
+# if __name__ == '__main__':
+   # print(main(1))
