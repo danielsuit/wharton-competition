a706eba9f372c4a72f69c992b9bcaf88f9d24fda
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-08-04 11:38:03 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
+import requests
+import csv
+import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 print("TensorFlow version:", tf.__version__)
 
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=APXD3W7Z5ODR41O4'
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
