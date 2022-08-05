75fba173836f31c009e7b3f11bec7f681af1ac7d
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-08-04 21:08:48 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 print("TensorFlow version:", tf.__version__)
-
 load_dotenv()
 key = os.environ.get('API_KEY')
-CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey='+key
-
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey='+key+'&datatype=csv'
 df = pd.read_csv(CSV_URL)
 print(df.head())
