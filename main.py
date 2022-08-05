07df31406202d1e6eda372bd5f84cb489904f9fa
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-08-04 20:54:54 -0400

    new file:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import tensorflow as tf
-import requests
-import csv
+import os
+from dotenv import load_dotenv
 import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 print("TensorFlow version:", tf.__version__)
 
-CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=APXD3W7Z5ODR41O4'
+load_dotenv()
+key = os.environ.get('API_KEY')
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey='+key
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    x = pd.DataFrame(data=my_list[1:], columns=my_list[0])
-    print(x)
-   #  for row in my_list:
-      #   print(row)
+df = pd.read_csv(CSV_URL)
+print(df.head())
