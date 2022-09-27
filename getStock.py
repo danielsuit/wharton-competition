5f1ecfa9f563280e6236132ede1fa2e1a04c2683
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-09-27 16:15:47 -0400

    modified:   __pycache__/currencyConversion.cpython-39.pyc 	modified:   __pycache__/exchangeConversion.cpython-39.pyc 	modified:   currencyConversion.py 	modified:   exchangeConversion.py 	modified:   getStock.py 	modified:   gpg.py 	modified:   stocklist.csv

--- getStock.py
+++ getStock.py
@@ -1,13 +1,64 @@
-import requests
-from bs4 import BeautifulSoup
-import os
-from dotenv import load_dotenv
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+#import os
+#from dotenv import load_dotenv
+#import numpy as np
+#import pandas as pd
+#from matplotlib import pyplot as plt
 import csv
 #stocks = pd.read_csv('stocklist.csv')
 #print(stocks.head)
+from bs4 import BeautifulSoup as BS
+import requests
+headers = {
+   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+}
+def exchangeConversion(exchange):
+   if exchange == 'B3 S.A.':
+      exchange = 'BVMF'
+   elif exchange == 'BSE LTD':
+      exchange = 'BOM'
+   elif exchange == 'Hong Kong Exchanges And Clearing Ltd':
+      exchange = 'HKG'
+   elif exchange == 'London Stock Exchange':
+      exchange = 'LON'
+   elif exchange == 'Nasdaq':
+      exchange = 'NASDAQ'
+   elif exchange == "New York Stock Exchange Inc.":
+      exchange = 'NYSE'
+   elif exchange == 'Shanghai Stock Exchange':
+      exchange = 'SHA'
+   elif exchange == 'Shenzhen Stock Exchange':
+      exchange = "SHE"
+   elif exchange == 'Toronto Stock Exchange':
+      exchange = "TSE"
+   return exchange
+def currencyConversion(price):
+   if price[0] == '$':
+      price = price.replace(',', '')
+      price = float(price[1:])
+   elif price[0] == '₹':
+      price = price.replace(',', '')
+      price = float(price[1:])*0.012
+   elif price[0:2] == 'R$':
+      price = price.replace(',', '')
+      price = float(price[2:])*0.19
+   elif price[0] == '¥':
+      price = price.replace(',', '')
+      price = float(price[1:])*0.14
+   elif price[0:3] == 'GBX':
+      price = price.replace(',','')
+      price = float(price[6:])*0.0113
+   return round(price, 2)
+def getPrice(ticker, exchange):
+   exchange = exchangeConversion(exchange)
+   url = 'https://www.google.com/finance/quote/{}:{}'
+   response = requests.get(url.format(ticker, exchange), headers=headers).text
+   soup = BS(response, 'lxml')
+   try:
+      match = soup.find('div', class_='YMlKec fxKbKc').text
+      match = currencyConversion(match)
+   except:
+      return 0
+   return match
 class GetStock:
    def __init__(self, number, name, ticker, exchange, industryGroup, industry, subIndustry, no):
       self.number = number;
@@ -37,6 +88,7 @@ def insertPrice(stocklist):
    for i in range(len(stockList)):
       price = getPrice(stockList[i][0], stocklist[i][2])  
       stockList[i] = [price, stockList[i][0], stockList[i][1], stockList[i][2], stockList[i][3], stockList[i][4], stockList[i][5], stockList[i][6]]
+      print(stockList[i])
    stockList.sort()
    print(stockList)
 insertPrice(stockList)
