4d4d815aaffc3cf79460d4cf8b494945e0a1a335
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-09-27 12:38:58 -0400

    modified:   __pycache__/currencyConversion.cpython-39.pyc 	modified:   __pycache__/gpg.cpython-39.pyc 	modified:   currencyConversion.py

--- currencyConversion.py
+++ currencyConversion.py
@@ -4,9 +4,9 @@ def currencyConversion(price):
       return price
    elif price[0] == '₹':
       price.replace(',', '')
+      print(price)
       price = float(price[1:])*0.012
       return price
    elif price[0:2] == 'R$':
       return float(price[2:0])*0.19
    return price
-   
