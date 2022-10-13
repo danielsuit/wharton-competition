e689625fc917cb91d39d415fca6823e3c21ad65f
Daniel S <106452651+danielsuit@users.noreply.github.com>
2022-10-13 14:56:07 -0400

    oaruwiowruorfauofewoafoiuouiooafiuaofnonafeooouoiuunawuofiuooiuouiaaaaaaaaaaafrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrfanonuofinoarioufrinfiufiufoiwu5oeriopiuporieupoesaiurpoaieurioaseurpoiuearwproaiuperoaisueaspoiurpeaoiuersapoiuaerpiouerapoiueraspoiuerapioreupoierampaioupameroiuvmioesurviouseviespmvuespoviupsemoivumpaoiseuvmpoaiseuriiprvsaeiruvmaoiseurpsvmpoiaeuvmpioauesvmpoirumspeovuiraouvoiesvmpoieruasmpvoiuermpoiavsmoivaiseuvpesuvmpoaieusrpvesmposeimauspoimvuproaisuriemvrumpimoiuvempiupoiupoiupoiupoiuwepoiurpoiwqeurpoiqwuerpoiqwuerpoqiuwerpioquwerpoiqwuerpoiquweriuwertyeroityoweiurtyowieurtyoiwuetyoiweurytoiuwerytoiuwyertiuwertiouywerotyerotyewortywoeriutyowieurtowiuertoiewurtyoiwerut

--- main.py
+++ main.py
@@ -28,6 +28,35 @@ class StockList:
       self.subIndustry = subIndustry;
    def __str__(self):
       return f"{self.number} | {self.name} | {self.ticker} | {self.exchange} | {self.sector} | {self.industryGroup} | {self.industry} | {self.subIndustry}";
+load_dotenv()
+key = os.environ.get('API_KEY')
+def historical(ticker):
+   url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&outputsize=full&apikey='+key+'&datatype=csv'
+   df = pd.read_csv(url)
+def close_hist(df):
+   plt.hist(df.close, bins=100)
+   plt.title("Closing Price Histogram")
+   plt.ylabel('Number of per bin')
+   plt.xlabel('Price')
+   print(plt.show())
+def open_hist(df):
+   plt.hist(df.open, bins=100)
+   plt.title("Opening Price Histogram")
+   plt.ylabel('Number of per bin')
+   plt.xlabel('Price')
+   print(plt.show())
+def low_hist(df):
+   plt.hist(df.low, bins=100)
+   plt.title("Day Low Histogram")
+   plt.ylabel('Number of per bin')
+   plt.xlabel('Price')
+   print(plt.show())
+def high_hist(df):
+   plt.hist(df.high, bins=100)
+   plt.title("Day High Histogram")
+   plt.ylabel('Number of per bin')
+   plt.xlabel('Price')
+   print(plt.show())
 def exchangeConversion(exchange):
    if exchange == 'B3 S.A.':
       return 'BVMF'
